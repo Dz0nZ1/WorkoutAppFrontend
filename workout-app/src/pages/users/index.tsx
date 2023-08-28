c13509ece997ff3def0889d663636a5e1b308eaa
Dz0nZ1 <dzonzi777@gmail.com>
2023-08-28 16:40:09 +0200

    Created home and users pages

---
+++ workout-app/src/pages/users/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function UserPage(){
+    return (
+        <>
+            <h1>User Page</h1>
+           <Link href={'/'}>Go to home page</Link>
+        </>
+    )
+}
