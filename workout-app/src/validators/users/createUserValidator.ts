a7b800658922cf8a16747a8ea33539c15d7b92ee
Dz0nZ1 <dzonzi777@gmail.com>
2023-08-28 21:36:47 +0200

    Created validation schema for user registration with yup

---
+++ workout-app/src/validators/users/createUserValidator.ts
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+
+export const createUserSchema = yup.object().shape({
+    firstName: yup.string().required().label("First name"),
+    lastName: yup.string().required().label("Last name"),
+    email: yup.string().email().required().label("Email"),
+    password: yup.string().required().label("Password"),
+}).required();
