483cd39737bc067f292f2d68e7a62853ae43d560
Dz0nZ1 <dzonzi777@gmail.com>
2023-08-28 20:55:13 +0200

    Created a post method for user requests

--- workout-app/src/data/index.ts
+++ workout-app/src/data/index.ts
@@ -1,8 +1,10 @@
-import {getDemo} from "@/data/http-client";
-import {QueryOptions, User} from "@/types";
+import {getDemo, postDemo} from "@/data/http-client";
+import {CreateUserQuery, QueryOptions, User} from "@/types";
 import {API_ENDPOINTS} from "@/data/endpoints";
 
 export const usersRequest = {
     all:({...query}: QueryOptions = {}) =>
-        getDemo<User[]>(API_ENDPOINTS.USERS_GET_ALL, {...query})
+        getDemo<User[]>(API_ENDPOINTS.USERS_GET_ALL, {...query}),
+    create: ({...query} : CreateUserQuery ) =>
+        postDemo<User>(API_ENDPOINTS.USERS_REGISTER, {...query})
 };
