6ae28ab53cce0596e949efc778131c0ffaf0e234
Dz0nZ1 <dzonzi777@gmail.com>
2023-08-28 17:40:36 +0200

    Edited pages

--- workout-app/src/pages/index.tsx
+++ workout-app/src/pages/index.tsx
@@ -1,38 +1,13 @@
 
 import Link from "next/link";
-import {useEffect, useState} from "react";
 
 export default function HomePage() {
-
-    const [counter, setCounter] = useState(0);
-    const [users, setUsers] = useState([]);
-
-
-    const fetchData = async () => {
-        const response = await fetch("http://localhost:8080/users/all");
-        const data = await response.json();
-        setUsers(data);
-    }
-
-    useEffect(() => {
-        fetchData();
-    }, [])
-
 
     return (
         <>
             <h1>HomePage</h1>
             <h2>Hello MiloNite</h2>
             <Link href={"/users"}>go to user page</Link>
-            <h1>{counter}</h1>
-            <button onClick={() => setCounter(counter => counter + 1)}>increment</button>
-            {users.map(user => {
-
-                return(
-                    // @ts-ignore
-                    <p key={user.id}>{user.firstName}</p>
-                )
-            })}
         </>
     )
 }
--- workout-app/src/pages/users/index.tsx
+++ workout-app/src/pages/users/index.tsx
@@ -1,6 +1,25 @@
 import Link from "next/link";
+import {useEffect, useState} from "react";
+import {usersRequest} from "@/data";
+import {User} from "@/types";
 
 export default function UserPage(){
+
+    const [users, setUsers] = useState<User>();
+
+
+    const fetchData = async () => {
+        const response = await usersRequest.all();
+        setUsers(response);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    console.log(users);
+
+
     return (
         <>
             <h1>User Page</h1>
