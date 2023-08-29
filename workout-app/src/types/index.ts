d8e619771cd0abb30e12681636c64e22e68bf98f
Dz0nZ1 <dzonzi777@gmail.com>
2023-08-29 16:04:35 +0200

    Updated types file with new interfaces

--- workout-app/src/types/index.ts
+++ workout-app/src/types/index.ts
@@ -1,9 +1,15 @@
+import {Dispatch} from "react";
+
+//User settings
+
 export interface User {
     id: number,
     firstName: string,
     lastName: string,
     email: string,
 }
+
+//Query settings
 
 export interface QueryOptions {
     pageIndex?: number,
@@ -15,4 +21,25 @@ export interface CreateUserQuery {
     lastName: string,
     email: string,
     password: string
+}
+
+
+//Context and reducer settings
+
+export interface UserSettingsContextProps {
+    state: User,
+    dispatch: Dispatch<UserSettingsAction>
+}
+
+export interface UserSettingsAction {
+    type: UserSettingsFunction,
+    payload: {
+        email?: string;
+        firstName? : string;
+    }
+}
+
+export enum UserSettingsFunction {
+    CHANGE_EMAIL = "CHANGE_EMAIL",
+    CHANGE_FIRST_NAME = "CHANGE_FIRST_NAME"
 }
