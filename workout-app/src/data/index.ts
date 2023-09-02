5d4109b4afb0c7a29f3b37d7e0614375d2dcbbc3
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-02 22:56:32 +0200

    Updated user request, added parameters in all function

--- workout-app/src/data/index.ts
+++ workout-app/src/data/index.ts
@@ -3,8 +3,8 @@ import {CreateUserQuery, QueryOptions, User} from "@/types";
 import {API_ENDPOINTS} from "@/data/endpoints";
 
 export const usersRequest = {
-    all:({...query}: QueryOptions = {}) =>
-        getDemo<User[]>(API_ENDPOINTS.USERS_GET_ALL, {...query}),
+    all:(query? : QueryOptions, config? : any) =>
+        getDemo<User[]>(API_ENDPOINTS.USERS_GET_ALL, {...query}, config),
     create: ({...query} : CreateUserQuery ) =>
         postDemo<User>(API_ENDPOINTS.USERS_REGISTER, {...query})
 };
