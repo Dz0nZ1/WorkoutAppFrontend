6c68ff420c306d3a0dc94409df66d2e4d1291214
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-02 22:51:01 +0200

    Deleted files for pages folder

--- workout-app/src/components/contexts/UserSettingsContext.tsx
+++
@@ -1,4 +0,0 @@
-import {createContext} from "react";
-import {UserSettingsContextProps} from "@/types";
-
-export const UserSettingsContext = createContext<UserSettingsContextProps | null>(null)
--- workout-app/src/hooks/useGetUsers.ts
+++ workout-app/src/hooks/useGetUsers.ts
@@ -1,17 +1,27 @@
 import useSWR from "swr";
 import {usersRequest} from "@/data";
 import {SWR_KEYS} from "@/data/swrKeys";
+import {useSession} from "next-auth/react";
 
 export const useGetUsers = () => {
 
+    const {data: session} = useSession();
+
+
+    const headers = {
+        // @ts-ignore
+        Authorization: `Bearer ${session?.user?.access_token}`
+    }
+
     const {data, error} = useSWR(
-        `${SWR_KEYS.USERS_GET_ALL}`, () => { return usersRequest.all()},
+        `${SWR_KEYS.USERS_GET_ALL}`, () => {
+            return usersRequest.all({}, {headers})},
         {
             refreshInterval: 90000,
-            revalidateIfStale: false,
-            revalidateOnFocus: false,
-            revalidateOn: false,
-            revalidateOnReconnect: false
+            revalidateIfStale: true,
+            revalidateOnFocus: true,
+            revalidateOn: true,
+            revalidateOnReconnect: true
         });
 
     return {data, error};
