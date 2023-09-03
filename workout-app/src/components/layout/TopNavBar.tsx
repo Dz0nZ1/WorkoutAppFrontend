7f120591ca9b542f36154c8dec8b2344728af82b
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-03 16:04:09 +0200

    Updated Nav bar

--- workout-app/src/components/layout/TopNavBar.tsx
+++ workout-app/src/components/layout/TopNavBar.tsx
@@ -1,9 +1,16 @@
 'use client'
 import {signIn, signOut, useSession} from "next-auth/react";
 import Link from "next/link";
+import {useState} from "react";
 
 export default function TopNavBar() {
     const {data: session} = useSession();
+
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!isMobileMenuOpen);
+    };
 
     return (
         <>
@@ -11,24 +18,162 @@ export default function TopNavBar() {
                 <div className="container mx-auto">
                     <div className="flex justify-between items-center">
                         <div className="text-white text-2xl font-bold">MiloNite</div>
-                        <ul className="flex space-x-4">
+                        {/* Mobile menu button */}
+                        <div className="lg:hidden">
+                            <button
+                                id="mobile-menu-button"
+                                className="text-white hover:text-gray-300"
+                                onClick={toggleMobileMenu}
+                            >
+                                <svg
+                                    className="h-6 w-6"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M4 6h16M4 12h16m-7 6h7"
+                                    ></path>
+                                </svg>
+                            </button>
+                        </div>
+                        {/* Desktop menu */}
+                        <ul className="hidden lg:flex space-x-4">
                             <button>
-                                <Link className="text-white hover:text-gray-300" href={"/"}>Home</Link>
+                                <Link
+                                    className="text-white hover:text-gray-300"
+                                    href={"/"}
+                                >
+                                    Home
+                                </Link>
                             </button>
                             <button>
-                                <Link className="text-white hover:text-gray-300" href={"/users"}>Users</Link>
+                                <Link
+                                    className="text-white hover:text-gray-300"
+                                    href={"/users"}
+                                >
+                                    Users
+                                </Link>
                             </button>
-                            {session == null ? (<button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded" onClick={() => signIn()}>Sign in</button>) : (
+                            <button>
+                                <Link
+                                    className="text-white hover:text-gray-300"
+                                    href={"/admin"}
+                                >
+                                    Admin
+                                </Link>
+                            </button>
+                            <button>
+                                <Link
+                                    className="text-white hover:text-gray-300"
+                                    href={"/employee"}
+                                >
+                                    Employee
+                                </Link>
+                            </button>
+                            {session == null ? (
+                                    <button
+                                        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+                                        onClick={() => signIn()}
+                                    >
+                                        Sign in
+                                    </button>
+                            ) : (
+
                                 <>
-                                    <button className="text-white hover:text-gray-300">{
-                                        //@ts-ignore
-                                        session.user?.firstName
-                                    }</button>
-                                    <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded" onClick={() => signOut()}>Sign out</button>
+                                    <button>
+                                        <Link
+                                            className="text-white hover:text-gray-300"
+                                            href={"/"}
+                                        >
+                                            {
+                                                //@ts-ignore
+                                                session.user?.firstName
+                                            }
+                                        </Link>
+                                    </button>
+
+                                    <button
+                                        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+                                        onClick={() => signOut()}
+                                    >
+                                        Sign out
+                                    </button>
                                 </>
                             )}
                         </ul>
                     </div>
+                    {/* Mobile menu */}
+                    {isMobileMenuOpen && (
+                        <ul className="lg:hidden bg-blue-500 mt-4">
+                            <li>
+                                <Link
+                                    className="text-white hover:text-gray-300"
+                                    href={"/"}
+                                >
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    className="text-white hover:text-gray-300"
+                                    href={"/users"}
+                                >
+                                    Users
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    className="text-white hover:text-gray-300"
+                                    href={"/admin"}
+                                >
+                                    Admin
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    className="text-white hover:text-gray-300"
+                                    href={"/employee"}
+                                >
+                                    Employee
+                                </Link>
+                            </li>
+                            {session == null ? (
+
+                                    <button
+                                        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+                                        onClick={() => signIn()}
+                                    >
+                                        Sign in
+                                    </button>
+
+                            ) : (
+                                   <>
+                                       <br/>
+                                       <hr/>
+                                       <Link
+                                           className="text-white hover:text-gray-300"
+                                           href={"/"}
+                                       >
+                                           {
+                                               //@ts-ignore
+                                               session.user?.firstName
+                                           }
+                                       </Link>
+                                       <button
+                                           className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+                                           onClick={() => signOut()}
+                                       >
+                                           Sign out
+                                       </button>
+                                   </>
+                            )}
+                        </ul>
+                    )}
                 </div>
             </nav>
         </>
