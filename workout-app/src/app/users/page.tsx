f3979a25db095cc7516043e37333a54ad01550e7
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-05 21:37:46 +0200

    Updated the user page

--- workout-app/src/app/users/page.tsx
+++ workout-app/src/app/users/page.tsx
@@ -1,10 +1,50 @@
+"use client"
+import {useSession} from "next-auth/react";
+import {useEffect, useState} from "react";
+import useAuth from "@/hooks/useAuth";
+import Loader from "@/components/ui/loader";
+
 export default function UserPage(){
 
-return (
-    <>
-        <h1 className={"text-3xl"} >Users page</h1>
-    </>
-)
+    const {data: session, status} = useSession();
+    const [users, setUsers] = useState();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState("");
 
+    const axiosAuth = useAuth();
+
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+           try {
+               setIsLoading(true);
+               setError("");
+               const res = await axiosAuth.get("http://localhost:8080/users/all");
+               console.log(res.status);
+               setUsers(res.data);
+           }catch (err){
+               // @ts-ignore
+               setError(err.message);
+           }finally {
+               setIsLoading(false);
+           }
+
+        };
+
+        fetchUsers();
+    }, [axiosAuth]);
+
+    return (
+        <>
+            {(isLoading || error) ? <Loader/> :
+                <>
+                    <h1>App users page</h1>
+                    {/*<button onClick={fetchUsers}>Get Users</button>*/}
+                    {/*{users && JSON.stringify(users)}*/}
+                </>
+            }
+            {users && JSON.stringify(users)}
+        </>
+    );
 
 }
