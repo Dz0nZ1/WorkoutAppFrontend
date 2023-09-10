3d81bd82c19fb9caf362ab82ed347f62b16ac995
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-10 02:35:35 +0200

    Created footer

---
+++ workout-app/src/components/ui/footer.tsx
@@ -0,0 +1,23 @@
+export default function Footer(){
+
+    return(
+        <footer className="bg-gray-50 py-5">
+            <div className="container mx-auto flex flex-col items-center justify-center text-center">
+                <p className="text-gray-600 text-sm sm:text-base">
+                    &copy; {new Date().getFullYear()} MiloNite. All Rights Reserved.
+                </p>
+                {/*<div className="flex mt-4">*/}
+                {/*    <a href="#" className="text-gray-400 hover:text-blue-500 px-2">*/}
+                {/*        Facebook*/}
+                {/*    </a>*/}
+                {/*    <a href="#" className="text-gray-400 hover:text-blue-500 px-2">*/}
+                {/*        Twitter*/}
+                {/*    </a>*/}
+                {/*    <a href="#" className="text-gray-400 hover:text-blue-500 px-2">*/}
+                {/*        Instagram*/}
+                {/*    </a>*/}
+                {/*</div>*/}
+            </div>
+        </footer>
+    )
+}
