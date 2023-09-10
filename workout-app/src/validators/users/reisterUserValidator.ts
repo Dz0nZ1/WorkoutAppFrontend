fc11029fc1516e694ebe514ad6f57edf649c8c45
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-10 13:33:44 +0200

    Updated user validator

--- workout-app/src/validators/users/reisterUserValidator.ts
+++ workout-app/src/validators/users/reisterUserValidator.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-export const createUserSchema = yup.object().shape({
+export const registerUserSchema = yup.object().shape({
     firstName: yup.string().required().label("First name"),
     lastName: yup.string().required().label("Last name"),
     email: yup.string().email().required().label("Email"),
