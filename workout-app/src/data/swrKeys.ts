24ff0498dff7c5243ffffe43f1a7e6c9fb02797d
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-15 12:27:01 +0200

    Updated swrKeys

--- workout-app/src/data/swrKeys.ts
+++ workout-app/src/data/swrKeys.ts
@@ -1,4 +1,23 @@
 export const SWR_KEYS = {
+
+   //users
    USERS_GET_ALL: 'getAllUsers',
    USERS_CREATE:  'createUsers',
+   USERS_REGISTER: "registerUser",
+
+   //exercises
+   EXERCISE_GET_ALL: "getAllExercises",
+   EXERCISE_GET: "getExercise",
+   EXERCISE_CREATE: "createExercise",
+   EXERCISE_UPDATE: "updateExercise",
+   EXERCISE_DELETE: "deleteExercise",
+
+   //plans
+   PLAN_GET_ALL: "getAllPlan",
+   PLAN_GET: "getPlan",
+   PLAN_CREATE: "createPlan",
+   PLAN_UPDATE: "updatePlan",
+   PLAN_DELETE: "deletePlan",
+   PLAN_GET_ALL_WITH_ID: "getAllPlanWithID"
+
 }
