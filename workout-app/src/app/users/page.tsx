11db083fcef9e165b9b647d09964735ef239c8fa
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-15 12:25:02 +0200

    Updated user page

--- workout-app/src/app/users/page.tsx
+++ workout-app/src/app/users/page.tsx
@@ -3,47 +3,52 @@ import {useSession} from "next-auth/react";
 import {useEffect, useState} from "react";
 import useAuth from "@/hooks/useAuth";
 import Loader from "@/components/ui/loader";
+import {useGetUsers} from "@/hooks/useGetUsers";
 
 export default function UserPage(){
 
     const {data: session, status} = useSession();
-    const [users, setUsers] = useState();
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [error, setError] = useState("");
+    // const [users, setUsers] = useState();
+    // const [isLoading, setIsLoading] = useState<boolean>(false);
+    // const [error, setError] = useState("");
 
-    const axiosAuth = useAuth();
+    const {data: users, isLoading, error} = useGetUsers();
 
-
-    useEffect(() => {
-        const fetchUsers = async () => {
-           try {
-               setIsLoading(true);
-               setError("");
-               const res = await axiosAuth.get("http://localhost:8080/users/all");
-               console.log(res.status);
-               setUsers(res.data);
-           }catch (err){
-               // @ts-ignore
-               setError(err.message);
-           }finally {
-               setIsLoading(false);
-           }
-
-        };
-
-        fetchUsers();
-    }, [axiosAuth]);
+    // const axiosAuth = useAuth();
+    //
+    //
+    // useEffect(() => {
+    //     const fetchUsers = async () => {
+    //        try {
+    //            setIsLoading(true);
+    //            setError("");
+    //            const res = await axiosAuth.get("http://localhost:8080/users/all");
+    //            console.log(res.status);
+    //            setUsers(res.data);
+    //        }catch (err){
+    //            // @ts-ignore
+    //            setError(err.message);
+    //        }finally {
+    //            setIsLoading(false);
+    //        }
+    //
+    //     };
+    //
+    //     fetchUsers();
+    // }, [axiosAuth]);
 
     return (
         <>
-            {(isLoading || error) ? <Loader/> :
-                <>
-                    <h1>App users page</h1>
-                    {/*<button onClick={fetchUsers}>Get Users</button>*/}
-                    {/*{users && JSON.stringify(users)}*/}
-                </>
-            }
-            {users && JSON.stringify(users)}
+            {isLoading || error ? <Loader/> : users && JSON.stringify(users)}
+
+            {/*{(isLoading || error) ? <Loader/> :*/}
+            {/*    <>*/}
+            {/*        <h1>App users page</h1>*/}
+            {/*        /!*<button onClick={fetchUsers}>Get Users</button>*!/*/}
+            {/*        /!*{users && JSON.stringify(users)}*!/*/}
+            {/*    </>*/}
+            {/*}*/}
+            {/*{users && JSON.stringify(users)}*/}
         </>
     );
 
