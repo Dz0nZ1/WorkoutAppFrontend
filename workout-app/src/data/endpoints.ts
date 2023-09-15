9ad06d9241c63b30bc833418eae1d66c1437fb5d
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-15 12:18:18 +0200

    Updated endpoints

--- workout-app/src/data/endpoints.ts
+++ workout-app/src/data/endpoints.ts
@@ -1,5 +1,28 @@
 export const API_ENDPOINTS = {
-    USERS_GET_ALL: "/users/all",
-    USERS_CREATE: "/users/create",
-    USERS_REGISTER: "/api/v1/auth/register"
+
+    //USER ENDPOINTS
+
+    USERS_GET_ALL: "/api/v1/admin/all",
+    USERS_CREATE: "/api/v1/admin/create",
+    USERS_REGISTER: "/api/v1/auth/register",
+    USERS_LOGIN: "/api/v1/auth/login",
+
+    //EXERCISE ENDPOINTS
+
+    EXERCISE_GET_ALL: "/api/v1/exercise/all",
+    EXERCISE_GET: "/api/v1/exercise/get/",
+    EXERCISE_CREATE: "/api/v1/exercise/create",
+    EXERCISE_UPDATE: "/api/v1/exercise/update/",
+    EXERCISE_DELETE: "/api/v1/exercise/delete/",
+
+
+    //PLAN ENDPOINTS
+
+    PLAN_GET_ALL: "/api/v1/plan/all",
+    PLAN_GET: "/api/v1/plan/get/",
+    PLAN_CREATE: "/api/v1/plan/create",
+    PLAN_UPDATE: "/api/v1/plan/update/",
+    PLAN_DELETE: "/api/v1/plan/delete/",
+    PLAN_GET_ALL_WITH_ID:"/api/v1/plan/user/"
+
 }
