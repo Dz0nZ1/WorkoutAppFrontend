0ccc28ba7f55f4c504078d517e1baee220670bdb
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-03 03:12:51 +0100

    Added a new module for session

--- workout-app/src/types/auth-problems.ts
+++ workout-app/src/types/auth-problems.ts
@@ -1,8 +1,25 @@
-export enum AuthProblems {
+import NextAuth from "next-auth/next";
 
+export enum AuthProblems {
     WRONG_PASSWORD = "Wrong password",
     USER_NOT_FOUND = "No such user exists",
-
-
-
 }
+
+declare module "next-auth" {
+    interface Session {
+        user : {
+            "email" : string,
+            "user_id" : number,
+            refreshToken: string,
+            access_token: string,
+            firstName: string,
+            lastName: string,
+            role: string,
+            iat: number,
+            exp: number,
+            jti: string
+
+        }
+    }
+}
+
