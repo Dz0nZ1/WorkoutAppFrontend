37ed4044f6f73cd16470d28c2ee64698c4d16924
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-03 03:01:09 +0100

    Updated SingleWorkout page with new design

--- workout-app/src/components/workout/SingleWorkout.tsx
+++ workout-app/src/components/workout/SingleWorkout.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {Button, Input} from "@nextui-org/react";
 
 const SingleWorkout = (props: any) => {
     const [properties, setProperties] = useState({});
@@ -8,48 +9,50 @@ const SingleWorkout = (props: any) => {
     };
 
     return (
-        <li key={props.index} className="mb-2">
-            {props.ex.name}
-            <input
-                type="number"
-                name="sets"
-                placeholder="sets"
-                className="border border-blue-200 rounded py-2 px-3 focus:outline-none focus:border-blue-400"
-                onChange={(e) =>
-                    setProperties((prevProp) => {
-                        return { ...prevProp, sets: e.target.value };
-                    })
-                }
-            />
-            <input
-                type="number"
-                name="reps"
-                placeholder="reps"
-                className="border border-blue-200 rounded py-2 px-3 focus:outline-none focus:border-blue-400"
-                onChange={(e) =>
-                    setProperties((prevProp) => {
-                        return { ...prevProp, reps: e.target.value };
-                    })
-                }
-            />
-            <input
-                type="number"
-                name="weight"
-                placeholder="weight"
-                className="border border-blue-200 rounded py-2 px-3 focus:outline-none focus:border-blue-400"
-                onChange={(e) =>
-                    setProperties((prevProp) => {
-                        return { ...prevProp, weight: e.target.value };
-                    })
-                }
-            />
-            <button
-                onClick={addToPlanHandler}
-                className="ml-2 bg-blue-400 text-white rounded px-3 py-1 focus:outline-none hover:bg-blue-600"
-            >
-                Add to plan
-            </button>
-        </li>
+        <>
+            <p className="mb-1">{props.ex.name}</p>
+            <li key={props.index} className="mb-2 flex flex-row w-full gap-x-7">
+                <Input
+                    type="number"
+                    name="sets"
+                    placeholder="sets"
+                    // className="border border-blue-200 rounded py-2 px-3 focus:outline-none focus:border-blue-400"
+                    onChange={(e) =>
+                        setProperties((prevProp) => {
+                            return { ...prevProp, sets: e.target.value };
+                        })
+                    }
+                />
+                <Input
+                    type="number"
+                    name="reps"
+                    placeholder="reps"
+                    // className="border border-blue-200 rounded py-2 px-3 focus:outline-none focus:border-blue-400"
+                    onChange={(e) =>
+                        setProperties((prevProp) => {
+                            return { ...prevProp, reps: e.target.value };
+                        })
+                    }
+                />
+                <Input
+                    type="number"
+                    name="weight"
+                    placeholder="weight"
+                    // className="border border-blue-200 rounded py-2 px-3 focus:outline-none focus:border-blue-400"
+                    onChange={(e) =>
+                        setProperties((prevProp) => {
+                            return { ...prevProp, weight: e.target.value };
+                        })
+                    }
+                />
+                <Button
+                    onClick={addToPlanHandler}
+                    className="ml-2 bg-blue-400 text-white rounded px-3 py-1 mt-2.5 focus:outline-none hover:bg-blue-600"
+                >
+                    Add
+                </Button>
+            </li>
+        </>
     );
 };
 
