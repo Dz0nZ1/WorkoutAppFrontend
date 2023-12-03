b4bdba8d4e83b9e3124e75d86b006be4d8314bff
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-03 03:12:08 +0100

    Updated createPlanForm with new design

--- workout-app/src/components/forms/createPlanForm.tsx
+++ workout-app/src/components/forms/createPlanForm.tsx
@@ -3,23 +3,20 @@
 import { useState } from "react";
 import { useGetExercises } from "@/hooks/useGetExercises";
 import { useSession } from "next-auth/react";
-import useAuth from "@/hooks/useAuth";
 import { useCreatePlan } from "@/hooks/useCreatePlan";
-import { API_ENDPOINTS } from "@/data/endpoints";
+import {Button, Input} from "@nextui-org/react";
+
 
 //components
 import SingleWorkout from "@/components/workout/SingleWorkout";
 import {Exercise, Property} from "@/types/entities";
-import {useCreateProperty} from "@/hooks/useCreateProperty";
-import {useGetPropertiesByName} from "@/hooks/useGetPropertiesByName";
-import {useDeletePropertyById} from "@/hooks/useDeletePropertyById";
+import toast from "react-hot-toast";
 
 export default function CreatePlanForm() {
     const { data: session } = useSession();
     const { data: exercises } = useGetExercises();
-
-
-    const [propertyList, setPropertyList] = useState([]);
+    const [disabled, setDisabled] = useState<boolean>(true)
+    const [propertyList, setPropertyList] = useState<Property[]>([]);
     const [exercisePlan, setExercisePlan] = useState([]);
 
 
@@ -32,119 +29,141 @@ export default function CreatePlanForm() {
     });
 
     //hook functions
-    const {createPlan, error: createPlanError, isLoading: createPlanLoading} = useCreatePlan()
-    const {createProperty, error: createPropertyError, isLoading: createPropertyLoading} = useCreateProperty();
-    const {getPropertiesByName, error: getPropertiesByNameError, isLoading:getPropertiesByNameIsLoading} = useGetPropertiesByName();
-    const {deleteProperty, error:deletePropertyError, isLoading: deletePropertyIsLoading } = useDeletePropertyById()
+    const {createPlan, error: createPlanError, isLoading: createPlanLoading} = useCreatePlan();
 
 
     const handleSubmit = async (e : any) : Promise<void> => {
         e.preventDefault();
-          await createPlan(formData);
-        alert("Plan has been created");
+        console.log(formData);
+        await toast.promise(createPlan(formData), {
+            loading: 'Loading',
+            success: 'Plan was successfully created',
+            error: 'Error creating a plan',
+        });
     };
 
     //This function deletes exercises from plan
-    const removeFromPlan = async (ex : any) : Promise<void> => {
+    const removeFromPlan =  (ex : any) : void => {
         const newPlan = exercisePlan.filter((exercise) => exercise !== ex);
         setExercisePlan(newPlan);
-        const newList = await getPropertiesByName(formData.name);
-        const deletedExercise =  newList.filter((prop : Property) => {
-            return prop.forExercise === ex.name;
-        });
-        await deleteProperty(deletedExercise[0].propertyId);
-        //
-        return;
+        const propList = propertyList.filter((prop : Property) => prop.forExercise !== ex.name);
+        setPropertyList(propList);
+        console.log(propertyList);
+        setDisabled(true);
+        toast.success("Removed from selection");
+
     };
 
     const handleAddExercises = async () : Promise<void> => {
-        const newList = await getPropertiesByName(formData.name);
         setFormData((prevFormData: any) => {
             return {
                 ...prevFormData,
                 // @ts-ignore
                 exercises: exercisePlan,
-                properties: newList,
+                properties: propertyList,
             };
         });
-        alert("Exercises are added to the plan");
+        setDisabled(false)
+        toast.success("Exercises are added to your plan");
     };
 
     return (
-        <div className="p-8">
-            <h2 className="text-blue-500 text-3xl font-bold mb-4">
-                Create a new training plan
-            </h2>
-            <div className="mb-8">
-                <label className="block text-gray-600 mb-2">Name:</label>
-                <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                    className="w-full border border-blue-200 rounded py-2 px-3 focus:outline-none focus:border-blue-400"
-                />
-                <h3 className="text-lg font-semibold mt-4 mb-1">
-                    Choose the exercises:
-                </h3>
-                <ul className="list-disc pl-6 mt-2">
-                    {exercises?.map((ex : Exercise, index : number) => (
-                        <SingleWorkout
-                            ex={ex}
-                            key={index}
-                            triggerUpdatePropertyList={(newProperty: any) => {
-                                // @ts-ignore
-                                setPropertyList((prevPropertyList) => {
-                                    return [...prevPropertyList, newProperty];
-                                });
+        <div className="container mx-auto">
+            <div className="p-8 shadow-2xl bg-gradient-to-r from-white to-gray-100">
+                <h1 className="text-5xl  text-blue-400 text-center mb-16">
+                    Create a new workout plan
+                </h1>
 
-                                const newProperties = { ...newProperty, forPlan: formData.name, forExercise: ex.name};
-                                createProperty(newProperties);
 
-                                // @ts-ignore
-                                setExercisePlan((prevPlan) => {
-                                    return [...prevPlan, ex];
-                                });
-                            }}
-                        />
-                    ))}
-                </ul>
-            </div>
-            <div>
-                <hr className="my-8" />
-                <h3 className="text-lg font-semibold">Current plan:</h3>
-                <ul className="list-disc pl-6 mt-2">
-                    {exercisePlan.map((ex : Exercise, index : number) => (
-                        <li key={index} className="mb-2">
-                            {ex.name}
-                            <button
-                                onClick={() => removeFromPlan(ex)}
-                                className="ml-2 bg-red-400 text-white rounded px-3 py-1 focus:outline-none hover:bg-red-600"
-                            >
-                                Remove
-                            </button>
-                        </li>
-                    ))}
-                </ul>
+                <div className="mb-8 flex justify-center flex-col items-center">
+                    {/*<label className="block text-gray-600 mb-2">Name:</label>*/}
+                    <Input
+                        label="Plan name"
+                        labelPlacement="outside"
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        className="w-1/2"
+                        size="lg"
+                        isRequired={true}
+                        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                        // className="w-full border border-blue-200 rounded py-2 px-3 focus:outline-none focus:border-blue-400"
+                    />
+                    <h3 className="text-lg font-semibold mt-4 mb-1">
+                        Choose the exercises:
+                    </h3>
+                    <ul className="list-disc pl-6 mt-2">
+                        {exercises?.map((ex : Exercise, index : number) => (
+                            <SingleWorkout
+                                ex={ex}
+                                key={index}
+                                triggerUpdatePropertyList={(newProperty: any) => {
+
+
+                                    // @ts-ignore
+                                    setPropertyList((prevPropertyList) => {
+                                        const newProperties = { ...newProperty, forExercise: ex.name};
+                                        return [...prevPropertyList, newProperties];
+                                    });
+
+                                    // @ts-ignore
+                                    setExercisePlan((prevPlan) => {
+                                        return [...prevPlan, ex];
+                                    });
+
+                                    console.log(propertyList);
+                                    toast.success("Exercise was added");
+
+                                }}
+                            />
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <hr className="my-8" />
+                    <h3 className="text-xl font-semibold text-center mb-12">Current plan</h3>
+                    <ul className="list-disc pl-6 mt-2  flex flex-col items-center">
+                        {exercisePlan.map((ex : Exercise, index : number) => (
+                            <li key={index} className="mb-2 flex flex-row justify-between gap-x-7 w-1/2">
+                                <p className="mt-1 text-lg gap-x-3">{ex.name}</p>
+                                <Button
+                                    onClick={() => removeFromPlan(ex)}
+                                    className="ml-2 bg-red-400 text-white rounded px-3 py-1 focus:outline-none hover:bg-red-600"
+                                >
+                                    Remove
+                                </Button>
+                            </li>
+                        ))}
+                    </ul>
+                    <br />
+                    <div className="flex justify-center">
+                        <Button
+                            size="lg"
+                            isDisabled={Object.keys(exercisePlan).length == 0 || !disabled}
+                            onClick={handleAddExercises}
+                            className="bg-green-400 text-white rounded px-4 py-2 mt-4 focus:outline-none hover:bg-green-600"
+                        >
+                            Add selected exercises to plan
+                        </Button>
+                    </div>
+                    <hr className="my-8" />
+                    <div className="flex justify-center">
+                        <Button
+                            size="lg"
+                            onClick={handleSubmit}
+                            isDisabled={disabled}
+                            className="bg-blue-400 text-white rounded px-4 py-2 focus:outline-none hover:bg-blue-600"
+                        >
+                            Create plan
+                        </Button>
+                    </div>
+                </div>
                 <br />
-                <button
-                    onClick={handleAddExercises}
-                    className="bg-green-400 text-white rounded px-4 py-2 mt-4 focus:outline-none hover:bg-green-600"
-                >
-                    Add selected exercises to plan
-                </button>
-                <hr className="my-8" />
-                <button
-                    onClick={handleSubmit}
-                    className="bg-blue-400 text-white rounded px-4 py-2 focus:outline-none hover:bg-blue-600"
-                >
-                    Create plan
-                </button>
+                <br />
+                <br />
+                <br />
             </div>
-            <br />
-            <br />
-            <br />
-            <br />
         </div>
+
     );
 }
